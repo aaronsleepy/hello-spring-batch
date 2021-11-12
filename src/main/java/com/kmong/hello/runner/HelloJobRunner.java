74ec17b2d2d2e8a8255939017a9d31aaf134c61b
aaronsleepy <aaron@kmong.com>
2021-11-12 14:30:47 +0900

    feature: add job launcher

---
+++ src/main/java/com/kmong/hello/runner/HelloJobRunner.java
@@ -0,0 +1,26 @@
+package com.kmong.hello.runner;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class HelloJobRunner implements ApplicationRunner {
+    private final JobLauncher jobLauncher;
+    private final Job helloJob;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "aaron")
+                .toJobParameters();
+
+        jobLauncher.run(helloJob, jobParameters);
+    }
+}
