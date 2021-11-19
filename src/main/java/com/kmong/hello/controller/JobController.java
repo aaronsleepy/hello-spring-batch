903320afadc1a2442cb83ea19d37df4b4faf5db3
aaronsleepy <aaron@kmong.com>
2021-11-19 15:22:34 +0900

    feature: 배치 비동기 실행 예제 추가

--- src/main/java/com/kmong/hello/controller/JobController.java
+++ src/main/java/com/kmong/hello/controller/JobController.java
@@ -4,10 +4,13 @@ import com.kmong.hello.vo.MemberRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobController {
     private final Job helloJob;
     private final JobLauncher jobLauncher;
+    private final BatchConfigurer batchConfigurer;
 
     @PostMapping("/hello")
     public String executeHelloJob(@RequestBody MemberRequest memberRequest) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
@@ -26,6 +30,21 @@ public class JobController {
                 .toJobParameters();
 
         JobExecution jobExecution = jobLauncher.run(helloJob, jobParameters);
+        log.info(String.format("job %s completed with %s", helloJob.getName(), jobExecution.getExitStatus().toString()));
+
+        return jobExecution.getExitStatus().toString();
+    }
+
+    @PostMapping("/hello/async")
+    public String executeHelloJobAsync(@RequestBody MemberRequest memberRequest) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", memberRequest.getName())
+                .toJobParameters();
+
+        SimpleJobLauncher simpleJobLauncher = (SimpleJobLauncher)batchConfigurer.getJobLauncher();
+        simpleJobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+
+        JobExecution jobExecution = simpleJobLauncher.run(helloJob, jobParameters);
         log.info(String.format("job %s completed with %s", helloJob.getName(), jobExecution.getExitStatus().toString()));
 
         return jobExecution.getExitStatus().toString();
