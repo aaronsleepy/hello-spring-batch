93bf29f9e012e9ca76f4fe42f50d8e7210206906
aaronsleepy <aaron@kmong.com>
2021-12-10 12:33:12 +0900

    feature: Delimiter 기반 FlaitFileItemReader 예제 추가

---
+++ src/main/java/com/kmong/hello/vo/CustomerVO.java
@@ -0,0 +1,16 @@
+package com.kmong.hello.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CustomerVO {
+    private String name;
+
+    private int age;
+
+    private String type;
+}
